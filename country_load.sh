3540386c3bea8390fc1bd2d7f56997b7741c816f
gabrielpc1190 <gabrielpc1190@gmail.com>
2020-08-11 11:18:06 -0600

    Update country_load.sh

--- country_load.sh
+++ country_load.sh
@@ -8,15 +8,16 @@ function loadcountry () {
         firewallGroupName=$1
         country=$2
 
-        echo "Downloading country definition for $country..." >> /var/log/country-load
+        logger -s "Downloading country definition for $country..."
         curl -o /config/user-data/${country}.cidr http://www.iwik.org/ipcountry/${country}.cidr
-        echo "Adding rules to firewall group $firewallGroupName..." >> /var/log/country-load
+        logger -s "Adding rules to firewall group $firewallGroupName..."
         for rule in `cat /config/user-data/${country}.cidr`; do
-                ipset add $firewallGroupName $rule
+            sudo ipset add $firewallGroupName $rule
         done
 }
 
-ipset -F $firewallGroupName
+sudo ipset -F $firewallGroupName
 for country in $countryList; do
         loadcountry $firewallGroupName $country
 done
+logger -s "The country_load.sh has been executed"
